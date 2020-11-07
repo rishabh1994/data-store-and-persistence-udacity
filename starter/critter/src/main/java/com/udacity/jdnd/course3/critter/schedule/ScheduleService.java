69815b462ad93e39e7b26ab50b406da711e37d9e
rishabhs1994 <rishu.sanklecha@gmail.com>
2020-11-07 23:46:12 +0530

    Add transactional annotation

--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/schedule/ScheduleService.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/schedule/ScheduleService.java
@@ -4,10 +4,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Service
 @Slf4j
+@Transactional
 public class ScheduleService {
 
     @Autowired
